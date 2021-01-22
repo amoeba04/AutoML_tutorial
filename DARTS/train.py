d32acdeab7a46b7e8f32040f66b9ac03a213d1ba
JaeSin <42027404+amoeba04@users.noreply.github.com>
2021-01-22 17:36:19 +0900

    testing...

--- DARTS/train.py
+++ DARTS/train.py
@@ -1,6 +1,10 @@
-import torch
 import sys
 import logging
+import argparse
+
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
 
 def main():
     if not torch.cuda.is_available():
@@ -8,8 +12,20 @@ def main():
         sys.exit(1)
     logger.info('GPU device available')
 
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.set_device(args.gpu)
+    torch.cuda.manual_seed(args.seed)
+    cudnn.benchmark = True
+    cudnn.enabled = True
+
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', type=int, default=0, help='random seed')
+    parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
+    args = parser.parse_args()
+
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
     stream_handler = logging.StreamHandler()
